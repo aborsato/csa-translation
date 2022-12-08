d2bcac67d37be14d62a2b955bb752b244bdc958f
Alan Borsato <37001712+aborsato@users.noreply.github.com>
2022-12-08 15:24:44 +0000

    quickstart code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# csa-translation
+# Azure Cognitive Services Translation demo
+
+from [Microsoft Learn](https://learn.microsoft.com/en-us/azure/cognitive-services/translator/quickstart-translator?tabs=python):
+
+Try the latest version of Azure Translator. In this quickstart, you'll get started using the Translator service to translate text using a programming language of your choice or the REST API. For this project, we recommend using the free pricing tier (F0), while you're learning the technology, and later upgrading to a paid tier for production.
+
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import json
+import uuid
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+# Add your key and endpoint
+key = os.getenv('TRANSLATOR_KEY')
+endpoint = os.getenv('TRANSLATOR_ENDPOINT')
+
+# location, also known as region.
+# required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+location = os.getenv('TRANSLATOR_LOCATION')
+
+PATH = '/translate'
+constructed_url = endpoint + PATH
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['fr', 'es', 'pt']
+}
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key,
+    # location required if you're using a multi-service or regional (not global) resource.
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+body = [{
+    'text': 'I would really like to drive your car around the block a few times!'
+}]
+
+request = requests.post(constructed_url, params=params, headers=headers, json=body, timeout=60)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
